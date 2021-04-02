0063a10d7c329fdc46ed276f60e35789b9e96409
Mikey Lemmon <mikey@thrivestone.com>
2021-04-02 21:08:58 +1300

    Add tailwind for handy styling classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,33 @@
 import React from 'react'
 import './App.css'
 
-const App : React.FC = () => {
-	// const video = useRef<HTMLVideoElement>(null)
-	// const canvas = useRef<HTMLCanvasElement>(null)
-	// const loop =
-	// useEffect(() => {
-	// 	if (!canvas.current || !video.current) {
-	// 		console.warn(`<App> Can't find canvas or video component reference`)
-	// 		return
-	// 	}
-	// 	// const vv = video.current as HTMLVideoElement
-	// 	// const cc = canvas.current as HTMLCanvasElement
-	// 	const ctx = canvas.current.getContext('2d')
-	// })
+const App: React.FC = () => {
 	return (
 		<div className='App'>
-			<div style={{
-				position: 'fixed',
-				top: 0,
-				left: 0,
-				right: 0,
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				textAlign: 'left',
-				flex: 1,
-				flexDirection: 'column',
-				// height: '15vh',
-				overflow: 'hidden',
-				// backgroundColor: 'rgba(0,0,0,0.4)',
-				backgroundColor: '#111',
-			}}>
-				<pre>
+			<div
+				className='bg-black bg-opacity-80 py-4 px-8 overflow-y-hidden fixed bottom-3/4 left-0 right-0 flex flex-col items-start justify-end'
+				style={{ minHeight: '25%' }}
+			>
+				<pre className='text-xl text-gray-300'>
+					<br />
 					<b>&gt;</b> # Hello world!
-					<br/><b>&gt;</b> # My name is Mikey
-					<br/><b>&gt;</b> # I'm into life
+					<br />
+					<b>&gt;</b> # My name is Mikey
+					<br />
+					<b>&gt;</b> # I'm into life
 				</pre>
 			</div>
 			<video
 				autoPlay={true}
 				loop={true}
-				style={{
-					position: 'fixed',
-					left: 0,
-					minWidth: '100vw',
-					minHeight: '100vh',
-					zIndex: -1,
-				}}
+				style={{ zIndex: -1 }}
+				className='fixed top-0 left-0 min-h-full min-w-full object-cover'
 			>
 				<source src='/videos/IMG_1391.mov' type='video/mp4' />
 			</video>
+			<div className='fixed top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center'>
+				<h1 className='text-white font-thick text-8xl'>Hello world!</h1>
+			</div>
 		</div>
 	)
 }
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans:
+				'SwedenSansRegular, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+			serif: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+			thick:
+				'SwedenSansBold, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+			mono:
+				'NotoMono, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
