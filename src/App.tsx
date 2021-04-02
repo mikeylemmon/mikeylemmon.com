4146acbbe1d659cc0cf32918286645dc7a1cd5e8
Mikey Lemmon <mikey@thrivestone.com>
2021-04-02 19:21:47 +1300

    Add goobers video and some "hello world" text

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App : React.FC = () => {
+	// const video = useRef<HTMLVideoElement>(null)
+	// const canvas = useRef<HTMLCanvasElement>(null)
+	// const loop =
+	// useEffect(() => {
+	// 	if (!canvas.current || !video.current) {
+	// 		console.warn(`<App> Can't find canvas or video component reference`)
+	// 		return
+	// 	}
+	// 	// const vv = video.current as HTMLVideoElement
+	// 	// const cc = canvas.current as HTMLCanvasElement
+	// 	const ctx = canvas.current.getContext('2d')
+	// })
+	return (
+		<div className='App'>
+			<div style={{
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				right: 0,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				textAlign: 'left',
+				flex: 1,
+				flexDirection: 'column',
+				// height: '15vh',
+				overflow: 'hidden',
+				// backgroundColor: 'rgba(0,0,0,0.4)',
+				backgroundColor: '#111',
+			}}>
+				<pre>
+					<b>&gt;</b> # Hello world!
+					<br/><b>&gt;</b> # My name is Mikey
+					<br/><b>&gt;</b> # I'm into life
+				</pre>
+			</div>
+			<video
+				autoPlay={true}
+				loop={true}
+				style={{
+					position: 'fixed',
+					left: 0,
+					minWidth: '100vw',
+					minHeight: '100vh',
+					zIndex: -1,
+				}}
+			>
+				<source src='/videos/IMG_1391.mov' type='video/mp4' />
+			</video>
+		</div>
+	)
 }
 
-export default App;
+export default App
